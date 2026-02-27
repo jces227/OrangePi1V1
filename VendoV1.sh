f8804fbefaebf3bcd1c7b40916c91111791e311b
jces227 <semillajohncrysler@gmail.com>
2026-02-28 00:10:44 +0800

    update commit

--- VendoV1.sh
+++ VendoV1.sh
@@ -8,7 +8,7 @@ sudo apt install apache2 php -y
 #sudo chown -R www-data:www-data /var/www/html/admin
 
 # Download files
-curl -L https://github.com/jces227/OrangePi1V1/tree/main/10-lan0.rules -o /etc/udev/rules.d/10-lan0.rules
+curl -L https://github.com/jces227/OrangePi1V1/blob/main/10-lan0.rules -o /etc/udev/rules.d/10-lan0.rules
 #curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/index.php -o /var/www/html/admin/index.php
 #curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/logout.php -o /var/www/html/admin/logout.php
 #curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/style.css -o /var/www/html/admin/style.css
