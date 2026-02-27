3371798fd9561e8ea158bbb361a1a0d5ffccb6e9
jces227 <semillajohncrysler@gmail.com>
2026-02-27 23:57:10 +0800

    update commit

--- VendoV1.sh
+++ VendoV1.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 echo "Updating system..."
-sudo apt update -y
+sudo apt update
+sudo apt install apache2 php -y
 
-# Target directory
-$Admin_DIR="/var/www/html/admin"
-
-# Create directory if it doesn't exist
-sudo mkdir $TARGET_DIR
-sudo chown -R www-data:www-data $Admin_DIR
+sudo mkdir /var/www/html/admin
+sudo chown -R www-data:www-data /var/www/html/admin
 
 # Download files
-curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/dashboard.php -o $Admin_DIR/dashboard.php
-curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/index.php -o $Admin_DIR/index.php
-curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/logout.php -o $Admin_DIR/logout.php
-curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/style.css -o $Admin_DIR/style.css
+curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/dashboard.php -o /var/www/html/admin/dashboard.php
+curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/index.php -o /var/www/html/admin/index.php
+curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/logout.php -o /var/www/html/admin/logout.php
+curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/style.css -o /var/www/html/admin/style.css
 
 echo "Done! Your website is installed."
 
