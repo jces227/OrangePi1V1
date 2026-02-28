264ef6c78c01c133cefc40f6939fe62b99125915
jces227 <semillajohncrysler@gmail.com>
2026-02-28 08:39:02 +0800

    update commit

--- VendoV1.sh
+++ VendoV1.sh
@@ -1,17 +1,59 @@
 #!/bin/bash
 
-echo "Updating system..."
-sudo apt update
-sudo apt install apache2 php -y
+echo "LAN Rules..."
+sudo curl -L https://raw.githubusercontent.com/jces227/OrangePi1V1/main/10-lan0.rules -o /etc/udev/rules.d/10-lan0.rules
+sudo udevadm control --reload
+
+echo "Updating System"
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y
+sudo apt install curl
+sudo apt install wget
+sudo apt install git
+sudo apt install unzip
+sudo apt install nano
+sudo apt install htop
+sudo apt install ca-certificates
+sudo apt install openssl
+sudo apt install gnupg
+sudo apt install lsb-release
+sudo apt install software-properties-common
+
+echo "Installing Ngix"
+sudo apt install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+sudo apt install php-fpm
+sudo apt install php-cli
+sudo apt install php-curl
+sudo apt install php-json
+sudo apt install php-mbstring
+sudo apt install php-xml
+sudo apt install php-zip
+
+pip3 install requests
+sudo apt install -y supervisor
+sudo apt install -y dphys-swapfile
+sudo systemctl enable dphys-swapfile
+
+echo "10-dhcp-all-interfaces.yaml Downloading..."
+sudo rm /etc/netplan/10-dhcp-all-interfaces.yaml
+sudo curl -L https://raw.githubusercontent.com/jces227/OrangePi1V1/main/10-dhcp-all-interfaces.yaml -o /etc/netplan/10-dhcp-all-interfaces.yaml
+
+
+
+
+
 
 #sudo mkdir /var/www/html/admin
 #sudo chown -R www-data:www-data /var/www/html/admin
 
 # Download files
-sudo curl -L https://raw.githubusercontent.com/jces227/OrangePi1V1/main/10-lan0.rules -o /etc/udev/rules.d/10-lan0.rules
-#curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/index.php -o /var/www/html/admin/index.php
-#curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/logout.php -o /var/www/html/admin/logout.php
-#curl -L https://github.com/jces227/OrangePi1V1/tree/main/admin/style.css -o /var/www/html/admin/style.css
+#sudo curl -L https://raw.githubusercontent.com/jces227/OrangePi1V1/tree/main/admin/dashboard.php -o /var/www/html/admin/dashboard.php
+#sudo curl -L https://raw.githubusercontent.com/jces227/OrangePi1V1/tree/main/admin/index.php -o /var/www/html/admin/index.php
+#sudo curl -L https://raw.githubusercontent.com/jces227/OrangePi1V1/tree/main/admin/logout.php -o /var/www/html/admin/logout.php
+#sudo curl -L https://raw.githubusercontent.com/jces227/OrangePi1V1/tree/main/admin/style.css -o /var/www/html/admin/style.css
 
 echo "Done! Your website is installed."
 
